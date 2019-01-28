3dfeadf9b90c421e73a430055e0b8856ecd95209
XiaoBai-2017 <15817223385@163.com>
2019-01-28 14:43:12 +0800

    .

--- exsample/1/main.cpp
+++ exsample/1/main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 struct bytedata
 {
@@ -6,13 +7,15 @@ struct bytedata
   unsigned:0;         /*无名位段，占0位，表下一位段从下一字边界开始*/
   unsigned bit_10:10; /*位段b，占10位*/
   int i;              /*成员i，从下一字边界开始*/
-}data_t;
+};
 
 int main(int argc, char const *argv[])
 {
-	printf("最大描述符数量(%d)\n", OGetMaxDescriptorNumber());
-	data_t dt;
-  printf("bit_2(%d)\n", dt.bit_2);
-  printf("%sizeof(data_t) (%d)\n", sizeof(data_t));
-	return 0;
+	bytedata dt = {0};
+  printf("amplitude before: bit_2(%d)\n", dt.bit_2);
+  dt.bit_2 = 10; // 超出范围，最大现实2
+  printf("amplitude after: bit_2(%d)\n", dt.bit_2);
+  printf("sizeof(bytedata) (%d)\n", sizeof(bytedata));
+	
+  return 0;
 }
